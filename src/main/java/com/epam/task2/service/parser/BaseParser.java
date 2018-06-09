ed8caf8b114037a194ac5d93b727d84980dafc23
allofapiece <stasana1998@tut.by>
2018-06-09 21:34:27 +0300

    Optimized application parsers

--- src/main/java/com/epam/task2/service/parser/BaseParser.java
+++ src/main/java/com/epam/task2/service/parser/BaseParser.java
@@ -1,13 +1,18 @@
 package com.epam.task2.service.parser;
 
 import com.epam.task2.entity.Composite;
-import com.epam.task2.entity.SyntaxUnit;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Implementation of {@link ParserInterface} and
+ * contains the default parse method
+ *
+ * @author Listratsenka Stanislau
+ * @version 1.0
+ */
 public abstract class BaseParser implements ParserInterface {
 	protected ParserInterface next;
 	protected String patternString;
@@ -18,6 +23,13 @@ public abstract class BaseParser implements ParserInterface {
 	 */
 	public BaseParser() {}
 
+	/**
+	 * Used pattern string from {@link ResourceBundle},
+	 * that can be defined in the child classes
+	 *
+	 * @param info String for parsing
+	 * @return Composite
+	 */
 	@Override
 	public Composite parse(String info) {
 		Pattern pattern = Pattern.compile(this.patternString); //Splitting string with regexp pattern

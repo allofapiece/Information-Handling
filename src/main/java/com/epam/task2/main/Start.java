aac86720147675dc99864ba6d1742625d7f8c72f
allofapiece <stasana1998@tut.by>
2018-06-09 21:36:44 +0300

    Create main proccessing class

--- src/main/java/com/epam/task2/main/Start.java
+++ src/main/java/com/epam/task2/main/Start.java
@@ -2,16 +2,22 @@ package com.epam.task2.main;
 
 import com.epam.task2.entity.Text;
 import com.epam.task2.entity.Word;
-import com.epam.task2.service.finder.UniqueWordFinder;
+import com.epam.task2.service.finder.*;
 import com.epam.task2.service.parser.TextParser;
 import com.epam.task2.service.reader.WholeTextFileReader;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.Logger;
 
 import java.io.File;
+import java.util.List;
 
+/**
+ * Class where application start from
+ *
+ * @author Listratsenka Stanislau
+ * @version 1.0
+ */
 public class Start {
-    private static Logger log = LogManager.getLogger(Start.class);
+    private static Logger log = Logger.getLogger(Start.class);
 
     public static void main(String[] args) {
         WholeTextFileReader reader = new WholeTextFileReader(new File("test.txt"));
@@ -20,7 +26,25 @@ public class Start {
         if (word != null) {
             log.info(word);
         } else {
-            log.error("First sentence does not has unique word");
+            log.error("First sentence does not has unique word.");
+        }
+
+        List<Word> words = new QuestionWordFinder().find(text, 5);
+        if (words != null) {
+            printWords(words);
+        } else {
+            log.error("Words of specific length did not find.");
+        }
+    }
+
+    /**
+     * Print list of words in console
+     *
+     * @param words
+     */
+    public static void printWords(List<Word> words) {
+        for (Word word : words) {
+            log.info(word.toString());
         }
     }
 }
